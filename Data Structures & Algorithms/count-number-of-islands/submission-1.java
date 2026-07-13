3cfd6c581796c2ba3df4aab5cdb85f0ed886e464
shiva mariswamy subramani <shivamar@users.noreply.github.com>
2026-07-12 23:07:52 -0400

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int numIslands(char[][] grid) {       
+        int[][] visited=new int[grid.length][grid[0].length];
+        int count=0;
+
+        for(int i=0;i < grid.length; i++){
+            for(int j=0; j < grid[0].length; j++){
+                if(grid[i][j] == '1'){
+                   if( visited[i][j] == 0){
+                        count++;
+                        dfs(grid,visited,i,j);
+                   }
+                }
+
+            }
+        }
+
+        return count;
+    }
+
+    public void dfs(char[][] grid, int[][] visited, int currR, int currC)
+    {
+        int[][]dirs = {{0,1},{1,0},{0,-1},{-1,0}}; //{1,1},{-1,-1},{1,-1},{-1,1}
+        visited[currR][currC] = 1;  
+
+        for(int[] dir : dirs){
+            if( Math.min(currR+dir[0], currC+dir[1]) == -1 || 
+            currR+dir[0] > grid.length-1 ||
+            currC+dir[1] > grid[0].length-1 || 
+            visited[currR+dir[0]][currC+dir[1]] == 1 ) continue;
+            else {
+                if(grid[currR+dir[0]][currC+dir[1]] == '1')
+                     dfs(grid, visited, currR+dir[0] , currC+dir[1]);
+            }
+        }
+    }
+
+}
