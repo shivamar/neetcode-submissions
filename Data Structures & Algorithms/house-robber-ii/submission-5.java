cb3cdaf6b8af4a2d9cc49adf421f1fec443838ee
shiva mariswamy subramani <shivamar@users.noreply.github.com>
2026-08-25 22:21:16 -0400

    Add: house-robber-ii - submission-5

---
+++ Data Structures & Algorithms/house-robber-ii/submission-5.java
@@ -0,0 +1,63 @@
+class Solution {
+    int dp[][];
+
+     public int rob1(int[] nums) {
+
+       if(nums.length==0) return 0;
+       if(nums.length==1) return nums[0];
+       if(nums.length==2) return Math.max(nums[0],nums[1]);
+
+        return Math.max(rob2(Arrays.copyOfRange(nums,0,nums.length-1)), rob2(Arrays.copyOfRange(nums,1,nums.length)));
+    }
+    public int rob2(int[] nums) {
+
+       int[] dp = new int[nums.length];
+       if(nums.length==0) return 0;
+       if(nums.length==1) return nums[0];
+       if(nums.length==2) return Math.max(nums[0],nums[1]);
+
+       dp[0]=nums[0];
+       dp[1]=Math.max(nums[0],nums[1]);
+
+       for(int i=2;i < nums.length;i++) {
+            dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i]);
+       }
+
+       return dp[nums.length-1];
+    }
+
+    // public int rob(int[] nums) {
+    //     dp = new int[nums.length];
+
+    //   return dfs(nums,0);
+    // }
+
+    // public int dfs(int[] nums, int index){
+
+    //     if(index > nums.length-1) return 0;
+
+    //     return Math.max(dfs(nums,index+1), nums[index] + dfs(nums,index+2));
+
+    // }
+
+    public int rob(int[] nums) {
+      dp = new int[nums.length][2];
+      
+      for(int[] row : dp)
+        Arrays.fill(row, -1);
+         
+      return dfs(nums,0,0);
+    }    
+
+    public int dfs(int[] nums, int index, int flag){
+        if(index > nums.length-1 || (flag==1 && index==nums.length-1)) return 0;
+
+        if(dp[index][flag] != -1) return dp[index][flag];
+
+        dp[index][flag] = Math.max(dfs(nums,index+1,flag), nums[index] + dfs(nums,index+2,(flag==1 || index==0) ? 1 : 0 ));
+        return dp[index][flag];
+
+    }
+
+
+}
