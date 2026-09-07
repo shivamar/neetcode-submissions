91ec62722ee5b4764190de3e1bc05d747e7023b3
shiva mariswamy subramani <shivamar@users.noreply.github.com>
2026-09-07 10:12:37 -0400

    Add: redundant-connection - submission-2

---
+++ Data Structures & Algorithms/redundant-connection/submission-2.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges){
+    HashMap<Integer,Integer> parent = new HashMap<Integer,Integer>();
+    //reverse(edges);
+    int n = edges.length;
+
+    for(int i=1; i< n+1; i++)
+    {
+        parent.put(i,i);
+    }
+
+    for(int[] edge : edges)
+    {
+        if(!union(edge[0],edge[1], parent))
+           return edge;
+    }
+
+    return null;
+
+    }
+
+    public int findRoot(int val, HashMap<Integer,Integer> parent)
+    {
+        int x=val;
+        while(parent.get(x)!=x) 
+            x=parent.get(x);
+
+        return x;
+    }
+
+    public boolean union (int a, int b, HashMap<Integer,Integer> parent)
+    {   
+        //System.out.println(" "+a+" "+b);
+        int rootA=findRoot(a, parent);
+        int rootB=findRoot(b, parent);
+       // System.out.println(" "+rootA+" "+rootB);
+
+        if(rootA==rootB) return false;
+        
+        parent.put(rootB,rootA);
+
+        return true;
+    }
+
+
+    public  void reverse(int[][] array) {
+    int start = 0;
+    int end = array.length - 1;
+    
+    while (start < end) {
+        // Swap elements
+        int[] temp = array[start];
+        array[start] = array[end];
+        array[end] = temp;
+        
+        // Move pointers toward the center
+        start++;
+        end--;
+    }
+}
+}
