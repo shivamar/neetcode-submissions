13e1ac7fa59852911f063380d3b5e60bf44a45a3
shiva mariswamy subramani <shivamar@users.noreply.github.com>
2026-09-10 14:30:58 -0400

    Add: missing-number - submission-11

---
+++ Data Structures & Algorithms/missing-number/submission-11.java
@@ -0,0 +1,60 @@
+class Solution {
+
+        public int missingNumber(int[] nums) {
+        int max = nums.length;
+        long sumOfRange = (max * (max+1)) / 2;
+
+        long sumActual=0;
+        for(int n : nums)
+        {
+           sumActual+=n;
+        }
+
+        int ans =  (int)  (sumOfRange - sumActual);
+        return ans;
+    }
+
+    public int missingNumber_nLogn1(int[] nums) {
+        Arrays.sort(nums);
+        int k=0;
+        for(int n : nums)
+        {
+           int xor = k ^ n;
+           if(xor != 0  ) return k;
+           k++;
+        }
+
+        return nums.length;
+    }
+
+    public int missingNumber_nLogn2(int[] nums) {
+    Arrays.sort(nums);
+    int k=0;
+
+    for(int n : nums)
+    {
+        int and = k & n;
+
+        if(and != n ) return k;
+
+        k++;
+    }
+
+    return nums.length;
+
+   }
+
+       public int missingNumber_n(int[] nums) {
+        int max = nums.length;
+        int bitMask=0;
+
+        for(int k=0; k <= max; k++)
+            bitMask = bitMask ^ k;
+
+        for(int n : nums)
+             bitMask = bitMask ^ n;
+
+        return bitMask;
+
+    }
+}
